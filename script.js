4e6f455b6ba475304887d978bd1ad03a9071c804
John Doe <149332198+kyungwan2023F@users.noreply.github.com>
2025-02-12 00:53:34 -0500

    Form submission and remove button

--- script.js
+++ script.js
@@ -16,25 +16,42 @@ function addBookToLibrary(title, author) {
   myLibrary.push(book);
 }
 
+function removeBook(index) {
+  myLibrary.splice(index, 1);
+  displayBook();
+}
+
 const container = document.getElementById("card-container");
 
 function displayBook() {
-    myLibrary.forEach((book) => {
-        // Build your card
-        const card = document.createElement("div");
-        card.classList.add("card");
-      
-        const heading = document.createElement("h2");
-        heading.textContent = book.title;
-        card.appendChild(heading);
-      
-        const author = document.createElement("h3");
-        author.textContent = `by ${book.author}`;
-        card.appendChild(author);
-      
-        // Append the card to the container
-        container.appendChild(card);
-      });
+  container.innerHTML = "";
+  myLibrary.forEach((book, index) => {
+    // Build your card
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    const heading = document.createElement("h2");
+    heading.textContent = book.title;
+    card.appendChild(heading);
+
+    const author = document.createElement("h3");
+    author.textContent = `by ${book.author}`;
+    card.appendChild(author);
+
+    const removeButton = document.createElement("button");
+    removeButton.textContent = "Remove";
+    removeButton.classList.add("remove-button");
+
+    removeButton.addEventListener("click", () => {
+      removeBook(index);
+    })
+    card.appendChild(removeButton);
+
+    
+
+    // Append the card to the container
+    container.appendChild(card);
+  });
 };
 
 addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald");
@@ -42,5 +59,33 @@ addBookToLibrary("1984", "George Orwell");
 addBookToLibrary("Brave New World", "Aldous Huxley");
 
 displayBook();
+
+const button = document.querySelector(".input-button");
+const dialog = document.getElementById("myDialog");
+const form = document.getElementById("book-form");
+const cancelButton = document.getElementById("cancel");
+
+button.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+
+  addBookToLibrary(title, author);
+  alert(`Submitted Book: ${document.getElementById("title").value} by ${document.getElementById("author").value}`);
+  dialog.close();
+  displayBook();
+});
+
+cancelButton.addEventListener("click", () => {
+  dialog.close();
+});
+
+
+
+
 
 
